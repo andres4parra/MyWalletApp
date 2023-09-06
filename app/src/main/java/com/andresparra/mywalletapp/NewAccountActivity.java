b9f93326a7d15e1818b4c0674a1802ea150d7838
andres4parra <116411931+andres4parra@users.noreply.github.com>
2023-09-06 09:56:12 -0500

    adelanto 6/09/2023

--- app/src/main/java/com/andresparra/mywalletapp/NewAccountActivity.java
+++ app/src/main/java/com/andresparra/mywalletapp/NewAccountActivity.java
@@ -2,13 +2,32 @@ package com.andresparra.mywalletapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NewAccountActivity extends AppCompatActivity {
+
+    private ImageView ivBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        ivBack= findViewById(R.id.iv_back);
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myintent = new Intent(NewAccountActivity.this, MainActivity.class);
+                startActivity(myintent);
+
+
+            }
+        });
+
     }
 }
