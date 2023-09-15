9bae711e3183bf750ceb6c28ddd4d87e36d9db46
andres4parra <116411931+andres4parra@users.noreply.github.com>
2023-09-15 15:11:57 -0500

    recycler view 15/9

---
+++ app/src/main/java/com/andresparra/mywalletapp/linearLayoutManager.java
@@ -0,0 +1,8 @@
+package com.andresparra.mywalletapp;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class linearLayoutManager extends RecyclerView.LayoutManager {
+    public linearLayoutManager(RecyclerView rvListAccount) {
+    }
+}
